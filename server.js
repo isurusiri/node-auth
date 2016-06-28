12af57de2b169daba12c7400b4104943ad3a5c95
isurusiri <isuruanatomy@gmail.com>
2016-06-28 22:02:24 +0530

    added auth service

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ var mongoose    = require('mongoose');
 var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
 var config = require('./config'); // get our config file
 var User   = require('./app/models/user'); // get our mongoose model
+
+var authServices = require('./app/services/auth-services');
     
 // =======================
 // configuration =========
@@ -111,40 +113,7 @@ apiRoutes.get('/users', function(req, res) {
 
 // route to authenticate a user (POST http://localhost:8080/api/authenticate)
 apiRoutes.post('/authenticate', function(req, res) {
-
-  // find the user
-  User.findOne({
-    name: req.body.name
-  }, function(err, user) {
-      
-    if (err) throw err;
-
-    if (!user) {
-      res.json({ success: false, message: 'Authentication failed. User not found.' });
-    } else if (user) {
-
-      // check if password matches
-      if (user.password != req.body.password) {
-        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
-      } else {
-
-        // if user is found and password is right
-        // create a token
-        var token = jwt.sign(user, app.get('superSecret'), {
-          expiresIn: 60*60*24 // expires in 24 hours
-        });
-
-        // return the information including token as JSON
-        res.json({
-          success: true,
-          message: 'Enjoy your token!',
-          token: token
-        });
-      }   
-
-    }
-
-  });
+  authServices.AuthenticateUser(req, res);    
 });
 
 // apply the routes to our application with the prefix /api
